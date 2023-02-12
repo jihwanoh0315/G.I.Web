620616579d266a53c76a6ed73f3faba8e73687ae
jihwanoh0315 <78191573+jihwanoh0315@users.noreply.github.com>
2023-02-12 00:24:50 -0800

    Add Education, Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,8 +38,69 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SERVICES MODAL ====================*/
 
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', ()=> {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+
+/*==================== PORTFOLIO MODAL ====================*/
+const prModalViews = document.querySelectorAll('.portfolio__modal'),
+      prModalBtns = document.querySelectorAll('.portfolio__modal-button'),
+      prModalCloses = document.querySelectorAll('.portfolio__modal-close')
+
+let prModal = function(prModalClick){
+    prModalViews[prModalClick].classList.add('active-modal')
+}
+
+prModalBtns.forEach((prModalBtn, i) => {
+    prModalBtn.addEventListener('click', ()=> {
+        prModal(i)
+    })
+})
+
+prModalCloses.forEach((prModalClose) => {
+    prModalClose.addEventListener('click', () => {
+        prModalViews.forEach((prModalView) => {
+            prModalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    spaceBetween: 48,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
+
 
 
 /*==================== TESTIMONIAL ====================*/
